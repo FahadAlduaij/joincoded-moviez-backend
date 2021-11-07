7e2136a11b04218f6c9180fbc5fa069066b295db
fahadalduaij <fahad_osama@hotmail.com>
2021-11-08 01:48:01 +0300

    test

--- db/models/Genre.js
+++ db/models/Genre.js
@@ -1,17 +1,20 @@
 const { Schema, model } = require("mongoose");
 
-const GenreSchema = Schema({
-    genreName: {
-        type: String,
-        unique: true,
-    },
-    movies: [{
-        type: Schema.Types.ObjectId,
-        ref: 'Movie'
-    }],
-    celebrities: [{type: Schema.Types.ObjectId, ref: 'Celebrity'}]
-},
-    {timestamps: true}
-)
+const GenreSchema = Schema(
+	{
+		genreName: {
+			type: String,
+			unique: true,
+		},
+		movies: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: "Movie",
+			},
+		],
+		celebrities: [{ type: Schema.Types.ObjectId, ref: "Celebrity" }],
+	},
+	{ timestamps: true }
+);
 
-module.exports = model("Genre", GenreSchema)
+module.exports = model("Genre", GenreSchema);
