8546512242ffcfc4516befe38042690f38cd0dfe
Alkandari-Y <yayalkandari@gmail.com>
2021-11-10 00:28:38 +0300

    added more stuff to celebs

--- apis/celebrity/celebrity.controllers.js
+++ apis/celebrity/celebrity.controllers.js
@@ -1,19 +1,23 @@
 const Celebrity = require("../../db/models/Celebrity");
 
 exports.fetchCelebrity = async (req, res, next) => {
-	try {
-		const celebrityList = await Celebrity.find().populate("movies");
-		return res.status(200).json(celebrityList);
-	} catch (error) {
-		next(error);
-	}
+  try {
+    const celebrityList = await Celebrity.find().populate("movies");
+    return res.status(200).json(celebrityList);
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.createCelebrity = async (req, res, next) => {
-	try {
-		const newCelebrity = await Celebrity.create(req.body);
-		res.status(201).json(newCelebrity);
-	} catch (error) {
-		next(error);
-	}
+  try {
+    if (req.file) {
+      req.body.image = `http://${req.get("host")}/media/${req.file.filename}`;
+    }
+    const newCelebrity = await Celebrity.create(req.body);
+    await newCelebrity.populate("movies");
+    res.status(201).json(newCelebrity);
+  } catch (error) {
+    next(error);
+  }
 };
--- apis/celebrity/celebrity.routes.js
+++ apis/celebrity/celebrity.routes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
-
+const passport = require("passport");
+const upload = require("../../middleware/multer");
+const { checkAdminUser } = require("../../middleware/checkAdminUser");
 const { fetchCelebrity, createCelebrity } = require("./celebrity.controllers");
 
 router.get("/", fetchCelebrity);
-router.post("/", createCelebrity);
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  upload.single("image"),
+  checkAdminUser,
+  createCelebrity
+);
 
 module.exports = router;
