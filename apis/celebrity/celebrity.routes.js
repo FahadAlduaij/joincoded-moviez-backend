d665cf3ff9b9f045577291d80c9e4f2e90d0238e
fahadalduaij <fahad_osama@hotmail.com>
2021-11-09 21:34:46 +0300

    creating celebrity

--- apis/celebrity/celebrity.routes.js
+++ apis/celebrity/celebrity.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { fetchCelebrity } = require("./celebrity.controllers");
+const { fetchCelebrity, createCelebrity } = require("./celebrity.controllers");
 
 router.get("/", fetchCelebrity);
+router.post("/", createCelebrity);
 
 module.exports = router;
