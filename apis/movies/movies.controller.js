20b71ac622e3934c9d4513e0c6331c6d9c4ba5ac
Alkandari-Y <yayalkandari@gmail.com>
2021-11-09 20:51:38 +0300

    populate changes in movies controller

--- apis/movies/movies.controller.js
+++ apis/movies/movies.controller.js
@@ -3,43 +3,42 @@ const Genre = require("../../db/models/Genre");
 const Celebrity = require("../../db/models/Celebrity");
 
 exports.getMovieList = async (req, res, next) => {
-	try {
-		const movieList = await Movie.find().populate({
-			path: "genres",
-			select: "genreName",
-		}).populate({ path: "celebrities", select: "name" });
-		return res.status(200).json(movieList);
-	} catch (error) {
-		next(error);
-	}
+  try {
+    const movieList = await Movie.find()
+      .populate("genres")
+      .populate("celebrities");
+    return res.status(200).json(movieList);
+  } catch (error) {
+    next(error);
+  }
 };
 
 exports.createMovie = async (req, res, next) => {
-	try {
-		if (req.file) {
-			req.body.image = `http://${req.get("host")}/media/${req.file.filename}`;
-		}
+  try {
+    if (req.file) {
+      req.body.image = `http://${req.get("host")}/media/${req.file.filename}`;
+    }
 
-		const newMovie = await Movie.create(req.body);
-		await newMovie.populate({ path: "genres", select: "name" });
-		await newMovie.populate({ path: "celebrities", select: "name" })
-		// Adding Created movies by id to genres
-		req.body.genres.forEach(async (genre) => {
-			await Genre.findByIdAndUpdate(
-				{ _id: genre._id },
-				{ $push: { movies: newMovie._id } }
-			);
-		});
+    const newMovie = await Movie.create(req.body);
+    await newMovie.populate("genres");
+    await newMovie.populate("celebrities");
+    // Adding Created movies by id to genres
+    req.body.genres.forEach(async (genre) => {
+      await Genre.findByIdAndUpdate(
+        { _id: genre._id },
+        { $push: { movies: newMovie._id } }
+      );
+    });
 
-		req.body.celebrities.forEach(async (celeb) => {
-			await Celebrity.findByIdAndUpdate(
-				{ _id: celeb._id },
-				{ $push: { movies: newMovie._id } }
-			);
-		});
+    req.body.celebrities.forEach(async (celeb) => {
+      await Celebrity.findByIdAndUpdate(
+        { _id: celeb._id },
+        { $push: { movies: newMovie._id } }
+      );
+    });
 
-		return res.status(201).json(newMovie);
-	} catch (error) {
-		next(error);
-	}
+    return res.status(201).json(newMovie);
+  } catch (error) {
+    next(error);
+  }
 };
