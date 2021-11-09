b3dd5d9e130babcee8f827be3e4552f3f776efe6
Alkandari-Y <yayalkandari@gmail.com>
2021-11-09 20:28:39 +0300

    fixed form data error

--- middleware/checkGenreForMovie.js
+++ middleware/checkGenreForMovie.js
@@ -4,6 +4,11 @@ exports.checkGenreForMovie = async (req, res, next) => {
   //Temporary array for genres and celebs id
   const genres = [];
   try {
+    //
+    if (req.body.genres.includes(",")) {
+      req.body.genres = req.body.genres.split(",");
+    }
+    console.log(`after slit `, req.body);
     //Checking for genres (string or array) and creating if they dont exist, then adding genre id to req.genres
     if (req.body.genres?.length >= 1) {
       if (typeof req.body.genres === "string") {
