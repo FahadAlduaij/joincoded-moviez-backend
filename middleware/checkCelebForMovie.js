97e09b12094c347c5049810942041d45ac1d8b2c
fahadalduaij <fahad_osama@hotmail.com>
2021-11-10 16:03:32 +0300

    fixing cele and genre middleware

--- middleware/checkCelebForMovie.js
+++ middleware/checkCelebForMovie.js
@@ -1,39 +1,42 @@
 const Celebrity = require("../db/models/Celebrity");
 
 exports.checkCelebForMovie = async (req, res, next) => {
-  //Temporary array for genres and celebs id
-  const genres = [];
-  try {
-    const celebrities = [];
+	//Temporary array for genres and celebs id
 
-    if (req.body.celebrities.includes(",")) {
-      req.body.celebrities = req.body.celebrities.split(",");
-    }
-    //Checking for celebrities (string or array) and creating if they dont exist, then adding celebrity id to req.celebrities
-    if (req.body.celebrities && req.body.celebrities.length >= 1) {
-      if (typeof req.body.celebrities === "string") {
-        const foundCeleb = await Celebrity.findOne({
-          name: `${req.body.celebrities.toUpperCase()}`,
-        });
-        if (!foundCeleb)
-          return next({ status: 404, message: "celebrity Not Found!" });
-        celebrities.push(foundCeleb._id);
-      } else {
-        for (const celebName of req.body.celebrities) {
-          celebName.toUpperCase();
-          const foundCeleb = await Celebrity.findOne({ name: `${celebName}` });
-          if (foundCeleb) {
-            celebrities.push(foundCeleb._id);
-          } else {
-            return next({ status: 404, message: "celebrities Not Found!" });
-          }
-        }
-      }
-    }
-    req.body.celebrities = celebrities;
-    next();
-  } catch (error) {
-    next(error);
-  }
-  //Check event planner for mongoose query expressions to simplify the code
+	try {
+		const celebrities = [];
+
+		if (
+			typeof req.body.celebrities === "string" &&
+			req.body.celebrities.includes(",")
+		) {
+			req.body.celebrities = req.body.celebrities.split(",");
+		}
+		//Checking for celebrities (string or array) and creating if they dont exist, then adding celebrity id to req.celebrities
+		if (req.body.celebrities && req.body.celebrities.length >= 1) {
+			if (typeof req.body.celebrities === "string") {
+				const foundCeleb = await Celebrity.findOne({
+					name: `${req.body.celebrities}`,
+				});
+				if (!foundCeleb)
+					return next({ status: 404, message: "celebrity Not Found!" });
+				celebrities.push(foundCeleb._id);
+			} else {
+				for (const celebName of req.body.celebrities) {
+					celebName;
+					const foundCeleb = await Celebrity.findOne({ name: `${celebName}` });
+					if (foundCeleb) {
+						celebrities.push(foundCeleb._id);
+					} else {
+						return next({ status: 404, message: "celebrities Not Found!" });
+					}
+				}
+			}
+		}
+		req.body.celebrities = celebrities;
+		next();
+	} catch (error) {
+		next(error);
+	}
+	//Check event planner for mongoose query expressions to simplify the code
 };
--- middleware/checkGenreForMovie.js
+++ middleware/checkGenreForMovie.js
@@ -1,38 +1,42 @@
 const Genre = require("../db/models/Genre");
 
 exports.checkGenreForMovie = async (req, res, next) => {
-  //Temporary array for genres and celebs id
-  const genres = [];
-  try {
-    //
-    if (req.body.genres.includes(",")) {
-      req.body.genres = req.body.genres.split(",");
-    }
-    console.log(`after slit `, req.body);
-    //Checking for genres (string or array) and creating if they dont exist, then adding genre id to req.genres
-    if (req.body.genres?.length >= 1) {
-      if (typeof req.body.genres === "string") {
-        const foundGenre = await Genre.findOne({
-          name: `${req.body.genres.toLowerCase()}`,
-        });
-        if (!foundGenre)
-          return next({ status: 404, message: "Genre Not Found!" });
-        genres.push(foundGenre._id);
-      } else {
-        for (const genreName of req.body.genres) {
-          genreName.toLowerCase();
-          const foundGenre = await Genre.findOne({ name: `${genreName}` });
-          if (foundGenre) {
-            genres.push(foundGenre._id);
-          } else {
-            return next({ status: 404, message: "Genres Not Found!" });
-          }
-        }
-      }
-    }
-    req.body.genres = genres;
-    next();
-  } catch (error) {
-    next(error);
-  }
+	//Temporary array for genres and celebs id
+	const genres = [];
+	try {
+		console.log("typeof", typeof req.body.genres);
+		//
+		if (
+			typeof req.body.genres === "string" &&
+			req.body.celebrities.includes(",")
+		) {
+			req.body.genres = req.body.genres.split(",");
+		}
+		console.log(`after slit `, req.body);
+		//Checking for genres (string or array) and creating if they dont exist, then adding genre id to req.genres
+		if (req.body.genres?.length >= 1) {
+			if (typeof req.body.genres === "string") {
+				const foundGenre = await Genre.findOne({
+					name: `${req.body.genres}`,
+				});
+				if (!foundGenre)
+					return next({ status: 404, message: "Genre Not Found!" });
+				genres.push(foundGenre._id);
+			} else {
+				for (const genreName of req.body.genres) {
+					genreName;
+					const foundGenre = await Genre.findOne({ name: `${genreName}` });
+					if (foundGenre) {
+						genres.push(foundGenre._id);
+					} else {
+						return next({ status: 404, message: "Genres Not Found!" });
+					}
+				}
+			}
+		}
+		req.body.genres = genres;
+		next();
+	} catch (error) {
+		next(error);
+	}
 };
