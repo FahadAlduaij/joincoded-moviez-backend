cb92443087ac4536318361a84b2fb62482dab88b
FAHADKHALDI <fahadalkhaldi96@gmail.com>
2021-11-10 18:28:39 +0300

    description

--- db/models/Movie.js
+++ db/models/Movie.js
@@ -2,28 +2,29 @@ const { Schema, model } = require("mongoose");
 const mongooseSlugPlugin = require("mongoose-slug-plugin");
 
 const MovieSchema = Schema(
-	{
-		title: {
-			type: String,
-		},
-		image: String,
-		releaseDate: {
-			type: Number,
-			min: 1850,
-			max: 2050,
-		},
-		genres: [
-			{
-				type: Schema.Types.ObjectId,
-				ref: "Genre",
-				required: true,
-			},
-		],
-		celebrities: [
-			{ type: Schema.Types.ObjectId, ref: "Celebrity", required: true },
-		],
-	},
-	{ timestamps: true }
+  {
+    title: {
+      type: String,
+    },
+    image: String,
+    releaseDate: {
+      type: Number,
+      min: 1850,
+      max: 2050,
+    },
+    genres: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Genre",
+        required: true,
+      },
+    ],
+    celebrities: [
+      { type: Schema.Types.ObjectId, ref: "Celebrity", required: true },
+    ],
+    description: String,
+  },
+  { timestamps: true }
 );
 //Check how to create ratings, one per user and calculate average... same with comments
 MovieSchema.plugin(mongooseSlugPlugin, { tmpl: "<%=title%>" });
