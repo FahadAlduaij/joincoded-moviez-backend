e8040a8b09008b1a0214af59676c0f205078f27c
fahadalduaij <fahad_osama@hotmail.com>
2021-11-10 17:22:52 +0300

    populate genres.controllers

--- apis/genres/genres.controllers.js
+++ apis/genres/genres.controllers.js
@@ -2,7 +2,7 @@ const Genre = require("../../db/models/Genre");
 
 exports.getGenreList = async (req, res, next) => {
     try {
-        const genreList = await Genre.find().populate('movies').populate('celebrities')
+        const genreList = await Genre.find().populate({path: "movies" , populate: {path: "genres" , populate: "movies"}}).populate('celebrities')
         return res.status(200).json(genreList)
     } catch (error) {
         next(error)
