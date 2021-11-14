9b8a3c2b9e462990a66ffbaf8d594ca17b4fbe00
fahadalduaij <fahad_osama@hotmail.com>
2021-11-14 05:00:13 +0300

    fix pap

--- apis/genres/genres.controllers.js
+++ apis/genres/genres.controllers.js
@@ -1,19 +1,21 @@
 const Genre = require("../../db/models/Genre");
 
 exports.getGenreList = async (req, res, next) => {
-    try {
-        const genreList = await Genre.find().populate({path: "movies" , populate: {path: "genres" , populate: "movies"}}).populate('celebrities')
-        return res.status(200).json(genreList)
-    } catch (error) {
-        next(error)
-    }
-}
+	try {
+		const genreList = await Genre.find()
+			.populate("movies")
+			.populate("celebrities");
+		return res.status(200).json(genreList);
+	} catch (error) {
+		next(error);
+	}
+};
 
 exports.createGenre = async (req, res, next) => {
-    try {
-        const newGenre = await Genre.create(req.body);
-        return res.status(201).json(newGenre)
-    } catch (error) {
-        next(error)
-    }
-}
+	try {
+		const newGenre = await Genre.create(req.body);
+		return res.status(201).json(newGenre);
+	} catch (error) {
+		next(error);
+	}
+};
--- middleware/checkGenreForMovie.js
+++ middleware/checkGenreForMovie.js
@@ -1,42 +1,43 @@
 const Genre = require("../db/models/Genre");
 
 exports.checkGenreForMovie = async (req, res, next) => {
-  //Temporary array for genres and celebs id
-  const genres = [];
-  try {
-    console.log("typeof", typeof req.body.genres);
-    //
-    if (
-      typeof req.body.genres === "string" &&
-      req.body.celebrities.includes(",")
-    ) {
-      req.body.genres = req.body.genres.split(",");
-    }
-    console.log(`after slit `, req.body);
-    //Checking for genres (string or array) and creating if they dont exist, then adding genre id to req.genres
-    if (req.body.celebrities && req.body.celebrities.length >= 1) {
-      if (typeof req.body.genres === "string") {
-        const foundGenre = await Genre.findOne({
-          name: `${req.body.genres}`,
-        });
-        if (!foundGenre)
-          return next({ status: 404, message: "Genre Not Found!" });
-        genres.push(foundGenre._id);
-      } else {
-        for (const genreName of req.body.genres) {
-          genreName;
-          const foundGenre = await Genre.findOne({ name: `${genreName}` });
-          if (foundGenre) {
-            genres.push(foundGenre._id);
-          } else {
-            return next({ status: 404, message: "Genres Not Found!" });
-          }
-        }
-      }
-    }
-    req.body.genres = genres;
-    next();
-  } catch (error) {
-    next(error);
-  }
+	//Temporary array for genres and celebs id
+
+	try {
+		const genres = [];
+
+		if (
+			typeof req.body.genres === "string" &&
+			req.body.genres.includes(",")
+		) {
+			req.body.genres = req.body.genres.split(",");
+		}
+		console.log(`After Split `, req.body);
+
+		//Checking for genres (string or array) and creating if they dont exist, then adding genre id to req.genres
+		if (req.body.genres && req.body.genres.length >= 1) {
+			if (typeof req.body.genres === "string") {
+				const foundGenre = await Genre.findOne({
+					name: `${req.body.genres}`,
+				});
+				if (!foundGenre)
+					return next({ status: 404, message: "Genre Not Found!" });
+				genres.push(foundGenre._id);
+			} else {
+				for (const genreName of req.body.genres) {
+					genreName;
+					const foundGenre = await Genre.findOne({ name: `${genreName}` });
+					if (foundGenre) {
+						genres.push(foundGenre._id);
+					} else {
+						return next({ status: 404, message: "Genres Not Found!" });
+					}
+				}
+			}
+		}
+		req.body.genres = genres;
+		next();
+	} catch (error) {
+		next(error);
+	}
 };
