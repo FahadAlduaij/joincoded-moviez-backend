ebe1eebf5ec1b385c01a2744dd528bbf61152aff
fahadalduaij <fahad_osama@hotmail.com>
2021-11-18 14:51:30 +0300

    Adding Delete Movie

--- apis/movies/movies.controller.js
+++ apis/movies/movies.controller.js
@@ -3,97 +3,87 @@ const Genre = require("../../db/models/Genre");
 const Celebrity = require("../../db/models/Celebrity");
 
 exports.findMovie = async (movieId, next) => {
-  try {
-    const foundMovie = await Movie.findById(movieId);
-    return foundMovie;
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const foundMovie = await Movie.findById(movieId);
+		return foundMovie;
+	} catch (error) {
+		next(error);
+	}
 };
 
 exports.getMovieList = async (req, res, next) => {
-  try {
-    const movieList = await Movie.find()
-      .populate("genres")
-      .populate("celebrities");
-    return res.status(200).json(movieList);
-  } catch (error) {
-    next(error);
-  }
+	try {
+		const movieList = await Movie.find()
+			.populate("genres")
+			.populate("celebrities");
+		return res.status(200).json(movieList);
+	} catch (error) {
+		next(error);
+	}
 };
 
 exports.createMovie = async (req, res, next) => {
-  try {
-    if (req.file) {
-      req.body.image = `http://${req.get("host")}/media/${req.file.filename}`;
-    }
+	try {
+		if (req.file) {
+			req.body.image = `http://${req.get("host")}/media/${req.file.filename}`;
+		}
 
-    const newMovie = await Movie.create(req.body);
-    await newMovie.populate("genres");
-    await newMovie.populate("celebrities");
-    // Adding Created movies by id to genres
-    req.body.genres.forEach(async (genre) => {
-      await Genre.findByIdAndUpdate(
-        { _id: genre._id },
-        { $push: { movies: newMovie._id } }
-      );
-    });
+		const newMovie = await Movie.create(req.body);
+		await newMovie.populate("genres");
+		await newMovie.populate("celebrities");
+		// Adding Created movies by id to genres
+		req.body.genres.forEach(async (genre) => {
+			await Genre.findByIdAndUpdate(
+				{ _id: genre._id },
+				{ $push: { movies: newMovie._id } }
+			);
+		});
 
-    req.body.celebrities.forEach(async (celeb) => {
-      await Celebrity.findByIdAndUpdate(
-        { _id: celeb._id },
-        { $push: { movies: newMovie._id } }
-      );
-    });
+		req.body.celebrities.forEach(async (celeb) => {
+			await Celebrity.findByIdAndUpdate(
+				{ _id: celeb._id },
+				{ $push: { movies: newMovie._id } }
+			);
+		});
 
-    return res.status(201).json(newMovie);
-  } catch (error) {
-    next(error);
-  }
+		return res.status(201).json(newMovie);
+	} catch (error) {
+		next(error);
+	}
 };
 
-//req.body
-// {
-//   movieId,
-//   userId,
-//   userRating
-// }
+exports.deleteMovie = async (req, res, next) => {
+	try {
+		if (!req.user.admin)
+			return next({
+				status: 401,
+				message: "Unauthorized to Delete",
+			});
 
-// exports.addMovieRating = async (req, res, next) => {
-//   try {
-//     const ratingsSum = req.movie.reduce()
-
-//     const userRatings = req.movie.length + 1;
-//     const ratings = req.movie
-//     const avgRating
-
-//     const updatedMovie = await Movie.findByIdAndUpdate(req.movie, req.body, {
-//       new: true,
-//       runValidators: true,
-//     });
-//     return res.status(200).json(updatedMovie);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
+		await Movie.deleteOne(req.movie);
+		return res.status(204).end();
+	} catch (error) {
+		next(error);
+	}
+};
 
 exports.addCommentToMovie = async (req, res, next) => {
-  try {
-    req.body.user = req.user._id;
-    req.body.username = req.user.username;
-    const updatedComments = await Movie.findByIdAndUpdate(
-      req.movie,
-      { $push: { comments: req.body } },
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
+	try {
+		req.body.user = req.user._id;
+		req.body.username = req.user.username;
+		const updatedComments = await Movie.findByIdAndUpdate(
+			req.movie,
+			{ $push: { comments: req.body } },
+			{
+				new: true,
+				runValidators: true,
+			}
+		);
 
-    return res
-      .status(200)
-      .json(updatedComments.comments[updatedComments.comments.length - 1]);
-  } catch (error) {
-    next(error);
-  }
+		return res
+			.status(200)
+			.json(updatedComments.comments[updatedComments.comments.length - 1]);
+	} catch (error) {
+		next(error);
+	}
 };
--- apis/movies/movies.routes.js
+++ apis/movies/movies.routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const passport = require("passport");
 const {
-  getMovieList,
-  createMovie,
-  addCommentToMovie,
-  findMovie,
+	getMovieList,
+	createMovie,
+	addCommentToMovie,
+	findMovie,
+	deleteMovie,
 } = require("./movies.controller");
 const router = express.Router();
 const upload = require("../../middleware/multer");
@@ -13,14 +14,14 @@ const { checkGenreForMovie } = require("../../middleware/checkGenreForMovie");
 const { checkCelebForMovie } = require("../../middleware/checkCelebForMovie");
 
 router.param("movieId", async (req, res, next, movieId) => {
-  const movie = await findMovie(movieId, next);
+	const movie = await findMovie(movieId, next);
 
-  if (movie) {
-    req.movie = movie;
-    next();
-  } else {
-    next({ status: 404, message: "Movie Not Found" });
-  }
+	if (movie) {
+		req.movie = movie;
+		next();
+	} else {
+		next({ status: 404, message: "Movie Not Found" });
+	}
 });
 
 // router.post(
@@ -30,21 +31,27 @@ router.param("movieId", async (req, res, next, movieId) => {
 // );
 
 router.post(
-  "/:movieId/comments",
-  passport.authenticate("jwt", { session: false }),
-  addCommentToMovie
+	"/:movieId/comments",
+	passport.authenticate("jwt", { session: false }),
+	addCommentToMovie
 );
 
 router.get("/", getMovieList);
 
 router.post(
-  "/",
-  passport.authenticate("jwt", { session: false }),
-  upload.single("image"),
-  checkAdminUser,
-  checkGenreForMovie,
-  checkCelebForMovie,
-  createMovie
+	"/",
+	passport.authenticate("jwt", { session: false }),
+	upload.single("image"),
+	checkAdminUser,
+	checkGenreForMovie,
+	checkCelebForMovie,
+	createMovie
+);
+
+router.delete(
+	"/:movieId",
+	passport.authenticate("jwt", { session: false }),
+	deleteMovie
 );
 
 module.exports = router;
